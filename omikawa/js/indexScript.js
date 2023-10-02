bc0172e1d50d52d3bd8f2e99bc50b979335285ad
attyoh <business.832@outlook.jpexit()>
2023-10-02 14:18:03 +0900

    treasurebox_effect_omikawa

---
+++ omikawa/js/indexScript.js
@@ -0,0 +1,37 @@
+function changeImageAndRedirect(event) {
+    const clickedBox = event.target;
+  
+    // 画像の透明度を変更して徐々に切り替えるエフェクト
+    clickedBox.style.opacity = 0;
+  
+    setTimeout(function () {
+      clickedBox.src = "img/treasureBox.jpg";
+      clickedBox.alt = "treasureBox";
+  
+      // 透明度を元に戻す
+      clickedBox.style.opacity = 1;
+    }, 1500);
+  
+    setTimeout(function () {
+      const birthdayUrl = "birthday.html";
+      window.location.href = birthdayUrl;
+    }, 2500);
+  }
+  
+  function changeImage(event) {
+    const clickedBox = event.target;
+    clickedBox.src = "img/openTreasureBox.jpg";
+    clickedBox.alt = "closeBox";
+  }
+  
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+  const boxNumber = "box" + randomNumber;
+  const randomBox = document.getElementById(boxNumber);
+  randomBox.addEventListener("click", changeImageAndRedirect);
+  
+  const imageBoxes = document.querySelectorAll(".image-box");
+  imageBoxes.forEach(function (box) {
+    if (box != randomBox) {
+      box.addEventListener("click", changeImage);
+    }
+  });
