fe36b9aec83f7071c65a836ca6b79b6a81ecf1a9
attyoh <business.832@outlook.jpexit()>
2023-10-02 13:43:01 +0900

    add open gradually

--- template/js/indexScript.js
+++ template/js/indexScript.js
@@ -1,10 +1,37 @@
-function changeImage(event) {
+function changeImageAndRedirect(event) {
+    const clickedBox = event.target;
+  
+    // 画像の透明度を変更して徐々に切り替えるエフェクト
+    clickedBox.style.opacity = 0;
+  
+    setTimeout(function () {
+      clickedBox.src = "img/treasureBox.jpg";
+      clickedBox.alt = "treasureBox";
+  
+      // 透明度を元に戻す
+      clickedBox.style.opacity = 1;
+    }, 2000);
+  
+    setTimeout(function () {
+      const birthdayUrl = "birthday.html";
+      window.location.href = birthdayUrl;
+    }, 3500);
+  }
+  
+  function changeImage(event) {
     const clickedBox = event.target;
     clickedBox.src = "img/openTreasureBox.jpg";
     clickedBox.alt = "closeBox";
-}
-
-const imageBoxes = document.querySelectorAll(".image-box");
-imageBoxes.forEach(function (box) {
-box.addEventListener("click", changeImage);
-});
+  }
+  
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+  const boxNumber = "box" + randomNumber;
+  const randomBox = document.getElementById(boxNumber);
+  randomBox.addEventListener("click", changeImageAndRedirect);
+  
+  const imageBoxes = document.querySelectorAll(".image-box");
+  imageBoxes.forEach(function (box) {
+    if (box != randomBox) {
+      box.addEventListener("click", changeImage);
+    }
+  });
